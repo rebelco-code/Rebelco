d8537f4f3600e5459f378a01ed62d1fca16e07af
Bertie <bertievbenecke2@gmail.com>
2026-02-22 20:12:34 +0200

    fix: add deployment workflow for GitHub Pages and set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://rebelco.co.za',
     output: 'static',
-    adapter: cloudflare()
 });
