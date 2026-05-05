fbe9261b9d87c07bb4db767c3094379b2d8593b9
kobussteynn <103757510+kobussteynn@users.noreply.github.com>
2026-05-05 14:03:28 +0200

    Fix home navbar positioning so transparency works

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import HomeBottomSection from "../components/home-bottom-section";
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-[#1f2023]">
-      <Navbar className="absolute inset-x-0 top-0 z-20 bg-transparent" />
+      <Navbar className="absolute inset-x-0 top-0 z-50 bg-transparent" />
       <main>
         <Gallery />
         <HomeBottomSection />
